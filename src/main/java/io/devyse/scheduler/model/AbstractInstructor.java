1a7b00e97d3b478b9377e48a701089a0d596cb5a
mikereinhold <mike@devyse.io>
2015-01-02 14:52:44 -0500

    Updated instructor and abstract instructor with basics.

--- src/main/java/io/devyse/scheduler/model/AbstractInstructor.java
+++ src/main/java/io/devyse/scheduler/model/AbstractInstructor.java
@@ -1,20 +1,114 @@
 /**
- * 
+ * @(#) AbstractInstructor.java
+ *
+ * This file is part of the Course Scheduler, an open source, cross platform
+ * course scheduling tool, configurable for most universities.
+ *
+ * Copyright (C) 2010-2014 Devyse.io; All rights reserved.
+ *
+ * @license GNU General Public License version 3 (GPLv3)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
  */
 package io.devyse.scheduler.model;
 
 /**
+ * Provide the basic and default implementation of the instructor interface
+ * 
  * @author Mike Reinhold
  * @since 4.12.8
  *
  */
-public abstract class AbstractInstructor {
+public abstract class AbstractInstructor implements Instructor {
 
 	/**
-	 * 
+	 * The instructor's name as presented by the university
 	 */
-	public AbstractInstructor() {
-		// TODO Auto-generated constructor stub
+	private String name;
+	
+	/**
+	 * The dataset of the Term which contains courses that reference the Instructor
+	 */
+	private TermDataSet termDataSet;
+	
+	/**
+	 * Construct a new abstract instructor using the specified name and term data set
+	 * 
+	 * @param name the instructor's name
+	 * @param termDataSet the dataset for the term containing the instructor
+	 */
+	protected AbstractInstructor() {
+		super();
+		
+		this.setName(name);
+		this.setTermDataSet(termDataSet);
 	}
 
+	/**
+	 * Set the instructor name
+	 * 
+	 * @param name the name to set
+	 */
+	protected void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Set the TermDataSet which contains this instructor
+	 * 
+	 * @param termDataSet the termDataSet to set
+	 */
+	protected void setTermDataSet(TermDataSet termDataSet) {
+		this.termDataSet = termDataSet;
+	}
+
+	/**
+	 * @return the instructor's name as formatted by the source university
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the termDataSet the data set from the term that contains the instructor
+	 */
+	public TermDataSet getTermDataSet() {
+		return termDataSet;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode(){
+		return this.getHashCode();
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object other){
+		if(other instanceof Instructor){ return this.equals((Instructor)other); }
+		else { return super.equals(other); }
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString(){
+		return Instructor.toString(this);
+	}
 }
--- src/main/java/io/devyse/scheduler/model/Instructor.java
+++ src/main/java/io/devyse/scheduler/model/Instructor.java
@@ -1,12 +1,32 @@
 /**
- * 
+ * @(#) Instructor.java
+ *
+ * This file is part of the Course Scheduler, an open source, cross platform
+ * course scheduling tool, configurable for most universities.
+ *
+ * Copyright (C) 2010-2014 Devyse.io; All rights reserved.
+ *
+ * @license GNU General Public License version 3 (GPLv3)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
  */
 package io.devyse.scheduler.model;
 
 import java.util.Objects;
 
 /**
- * Represent an instructor
+ * Represent an instructor that teaches a course at a university
  * 
  * @author Mike Reinhold
  * @since 4.12.8
@@ -36,8 +56,9 @@ public interface Instructor extends Comparable<Instructor> {
 	/* (non-Javadoc)
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
-	public default boolean equals(Term other) {
-		return	false//TODO equals stub
+	public default boolean equals(Instructor other) {
+		return	this.getTermDataSet().equals(other.getTermDataSet()) &&
+				this.getName().equals(other.getName())
 		;
 	}
 	
@@ -46,7 +67,8 @@ public interface Instructor extends Comparable<Instructor> {
 	 */
 	public default int getHashCode() {
 		return Objects.hash(
-				//TODO hash stub
+			this.getTermDataSet(),
+			this.getName()
 		);
 	}
 	
@@ -54,15 +76,25 @@ public interface Instructor extends Comparable<Instructor> {
 	 * @see java.lang.Comparable#compareTo(java.lang.Object)
 	 */
 	public default int compareTo(Instructor other) {
-		//TODO method stub
-		return 0;
+		int result = this.getTermDataSet().compareTo(other.getTermDataSet());
+		
+		if(result == 0){
+			result = this.getName().compareTo(other.getName());
+		}
+		
+		return result;
 	}
 	
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
-	public static String toString(Term term){
-		//TODO method stub
-		return null;
+	public static String toString(Instructor instructor){
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(instructor.getTermDataSet());
+		sb.append(" - ");
+		sb.append(instructor.getName());
+		
+		return sb.toString();
 	}
 }
